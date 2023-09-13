a86310a3d2dbd72bacf65e6e8621f2c80ea944bc
bandit-ibayashi <hirofumi.ibayashi@star-m.jp>
2023-09-13 16:36:09 +0900

    Deleted unnecessary files.

--- software/examples/1_StarIODeviceSetting/Pods/StarIO/StarIO.xcframework/ios-arm64/StarIO.framework/Headers/Platform.h
+++
@@ -1,7 +0,0 @@
-#ifndef platform__
-#define platform__
-
-#include <StarIO/Mac.h>
-
-#endif
-
--- software/libs/StarIODeviceSetting.xcframework/ios-arm64_i386_x86_64-simulator/StarIODeviceSetting.framework/Versions/A/Headers/StarIODeviceSetting.h
+++
@@ -1,9 +0,0 @@
-//
-//  StarIODeviceSetting.h
-//  StarIODeviceSetting
-//
-
-#import <StarIODeviceSetting/Internal/SMNetworkManager.h>
-#import <StarIODeviceSetting/Internal/SMNetworkSetting.h>
-#import <StarIODeviceSetting/Internal/SMSteadyLANSetting.h>
-
